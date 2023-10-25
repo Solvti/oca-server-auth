ed63011515936109dd9d23c135b7aafeaaf527b2
Stefan W <stefan.wiselka@solvti.pl>
2023-10-25 11:33:13 +0200

    [FIX] tests

--- auth_api_key_hash/tests/test_auth_api_key_hash.py
+++ auth_api_key_hash/tests/test_auth_api_key_hash.py
@@ -1,26 +1,19 @@
 # Copyright 2023 Solvti Sp. z o.o.
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
 from odoo.exceptions import AccessError, ValidationError
 from odoo.tests.common import SavepointCase
 
-from odoo.addons.server_environment import serv_config
 
-
-class TestAuthApiKey(SavepointCase):
+class TestAuthApiKeyHash(SavepointCase):
     @classmethod
     def setUpClass(cls, *args, **kwargs):
         super().setUpClass(*args, **kwargs)
-        cls.AuthApiKey = cls.env["auth.api.key.hash"]
+        cls.AuthApiKeyHash = cls.env["auth.api.key.hash"]
         cls.demo_user = cls.env.ref("base.user_demo")
-        cls.api_key_good = cls.AuthApiKey.create(
+        cls.api_key_good = cls.AuthApiKeyHash.create(
             {"name": "good", "user_id": cls.demo_user.id, "key": "api_key"}
         )
-        cls.api_key_from_env = cls.AuthApiKey.create(
-            {"name": "from_env", "key": "dummy", "user_id": cls.demo_user.id}
-        )
-        cls.api_key_from_env.refresh()
-        serv_config.add_section("api_key_from_env")
-        serv_config.set("api_key_from_env", "key", "api_key_from_env")
 
     def test_lookup_key_from_db(self):
         demo_user = self.env.ref("base.user_demo")
@@ -28,18 +21,6 @@ class TestAuthApiKey(SavepointCase):
             self.env["auth.api.key.hash"]._retrieve_uid_from_api_key("api_key"),
             demo_user.id,
         )
-
-    def test_lookup_key_from_env(self):
-        self.assertEqual(
-            self.env["auth.api.key.hash"]._retrieve_uid_from_api_key(
-                "api_key_from_env"
-            ),
-            self.demo_user.id,
-        )
-        with self.assertRaises(ValidationError):
-            # dummy key must be replace with the one from env and
-            # therefore should be unusable
-            self.env["auth.api.key.hash"]._retrieve_uid_from_api_key("dummy")
 
     def test_wrong_key(self):
         with self.assertRaises(ValidationError), self.env.cr.savepoint():
@@ -52,15 +33,10 @@ class TestAuthApiKey(SavepointCase):
                 user=self.demo_user
             )._retrieve_uid_from_api_key("api_wrong_key")
 
-    def test_cache_invalidation(self):
+    def test_no_edit_key(self):
         self.assertEqual(
             self.env["auth.api.key.hash"]._retrieve_uid_from_api_key("api_key"),
             self.demo_user.id,
         )
-        self.api_key_good.write({"key": "updated_key"})
-        self.assertEqual(
-            self.env["auth.api.key.hash"]._retrieve_uid_from_api_key("updated_key"),
-            self.demo_user.id,
-        )
         with self.assertRaises(ValidationError):
-            self.env["auth.api.key.hash"]._retrieve_uid_from_api_key("api_key")
+            self.api_key_good.write({"key": "updated_key"})
